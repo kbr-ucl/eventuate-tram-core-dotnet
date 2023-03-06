34fb7ec1f603e7dab095b4a8bc359fd1a7f3fc74
KBR <kbr@ucl.dk>
2023-03-06 07:47:57 +0100

    Upgrade from depricated function

--- IO.Eventuate.Tram.IntegrationTests/TestHelpers/DynamicEventuateSchemaModelCacheKeyFactory.cs
+++ IO.Eventuate.Tram.IntegrationTests/TestHelpers/DynamicEventuateSchemaModelCacheKeyFactory.cs
@@ -9,15 +9,6 @@ namespace IO.Eventuate.Tram.IntegrationTests.TestHelpers
 	/// Otherwise, the first model created is cached and used even if the EventuateSchema has changed
 	/// </summary>
 	public class DynamicEventuateSchemaModelCacheKeyFactory : IModelCacheKeyFactory {
-		//	public object Create(DbContext context)
-		//	{
-		//		if (context is EventuateTramDbContext eventuateTramDbContext)
-		//		{
-		//			return (context.GetType(), eventuateTramDbContext.EventuateDatabaseSchema);
-		//		}
-		//		return context.GetType();
-		//	}
-
 		/// <summary>
 		/// Used to ensure a new model is created for each different EventuateSchema
 		/// Otherwise, the first model created is cached and used even if the EventuateSchema has changed
